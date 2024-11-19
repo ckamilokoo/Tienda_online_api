a6eaef79cc392baf1e3604558fb4c769ee260e7d
ckamilokoo <camilo.camposog@gmail.com>
2024-11-19 05:16:44 -0300

    c

--- tienda_api/database.py
+++ tienda_api/database.py
@@ -11,6 +11,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Dependency
+#hola
 def get_db():
     db = SessionLocal()
     try:
